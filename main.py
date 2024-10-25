5428982fded0fca46a6356f001e7ae8b5a865c6f
k-malek <krzysztof.malek.90@gmail.com>
2024-10-25 11:35:20 +0200

    list of transactions by date - creating getter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
-from data_entry import get_amount,get_category,get_date,get_description
+from data_entry import get_amount,get_category,get_date,get_description,DATE_FORMAT
 
 class Csv:
     '''Csv data handler class'''
@@ -30,6 +30,28 @@ class Csv:
             writer = csv.DictWriter(csv_file,fieldnames=cls.FIELDNAMES)
             writer.writerow(new_entry)
 
+    @classmethod
+    def get_transactions_by_date(cls,start_date:str,end_date:str):
+        df = pd.read_csv(cls.CSV_FILE)
+        df['date'] = pd.to_datetime(df['date'],format=DATE_FORMAT)
+        start_date = datetime.strptime(start_date,DATE_FORMAT)
+        end_date = datetime.strptime(end_date,DATE_FORMAT)
+
+        mask = (df['date'] >= start_date) & (df['date'] <= end_date)
+
+        results_df = df.loc[mask]
+
+        if results_df.empty:
+            print('No transactions found for given date frame')
+        else:
+            print(f'Transactions for range: {start_date.strftime(DATE_FORMAT)} - {end_date.strftime(DATE_FORMAT)}')
+            print(
+                results_df.to_string(
+                    index=False,
+                    formatters={'date': lambda x: x.strftime(DATE_FORMAT)}
+                    )
+                )
+
 def add_datarow():
     Csv.initialize_csv()
     date=get_date("Provide transaction date in dd-mm-yyyy format: ")
@@ -38,4 +60,4 @@ def add_datarow():
     description=get_description()
     Csv.add_entry(date,amount,category,description)
 
-add_datarow()
+Csv.get_transactions_by_date('12-10-2001','12-10-2001')
